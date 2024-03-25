09faf56b01a58c244bac45eec3fb87652c4762aa
Ramareddy <ramareddy9652@gmail.com>
2024-03-25 11:07:14 +0530

    done changes

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
