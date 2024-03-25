e8c9486db94d589ee86db9a9cc49cd92ea16de09
Ramareddy <ramareddy9652@gmail.com>
2024-03-25 10:58:53 +0530

    first commit

---
+++ src/core/layout/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+export const Layout = (props) => {
+
+  const { children } = props;
+
+  return (
+    <>
+      <header>
+        <div className="d-flex bg-gradient">
+          <div className="d-flex flex-grow-1">
+            <div className="flex-grow-1">
+              <div className="ReactTitle ps-3  d-flex align-items-center">
+                <img src={require("../../react-2@2x.png")} alt="logo" />
+                <h3 className="mb-0">Reactjs App with API</h3>
+              </div>
+              <div className="d-flex ps-3 align-items-center">
+                <div className="flex-grow-1">
+                  <h4>Weather</h4>
+                  <p className="pb-0 mb-0">
+                    Purpose: API is used to access current weather data for any location
+                  </p>
+                </div>
+                <div className="link me-3">
+                  <p className="mb-0 py-0">
+                    {" "}
+                    <a href="https://openweathermap.org/api">
+                    https://openweathermap.org/api
+                    </a>
+                    <img
+                      src={require("../../noun-link.png")}
+                      className="ms-2"
+                    />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main role="main" className="container pt-5">
+        {children}
+      </main>
+
+      <footer className="footer footer-dark">
+        
+      </footer>
+    </>
+  )
+}
